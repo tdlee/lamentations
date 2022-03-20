38a09f6e098d6fe2ac11c15e16916114952b9658
David Lee <t.d.lee@servicemusic.org.uk>
2022-03-20 15:03:27 +0000

    Intro: clarify that qinah rhythm is in the first four chapters, not the final.

--- index.php
+++ index.php
@@ -160,7 +160,7 @@
 
     <p>
     Declaim it aloud!
-    Vital to the Hebrew verse of Lamentations is its three-two <i>qinah</i> stress pattern.
+    Vital to the Hebrew verse of Lamentations is its three-two <i>qinah</i> stress pattern pervading all but the final chapter.
     Hebrew poetry can be tightly concise compared to English equivalents
     which tend to wax more loquaciously expansive.
     This version aims for that original tight conciseness.
