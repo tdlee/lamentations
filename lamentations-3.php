f3c00dfb34cca722a0e4048bb856af2c300f23a7
David Lee <t.d.lee@servicemusic.org.uk>
2022-05-22 16:05:29 +0100

    3:59: typo (lower/upper-case "I")

--- lamentations-3.php
+++ lamentations-3.php
@@ -378,7 +378,7 @@
          you redeemed my life.
       </p>
       <p>
-      Witness, O <span class="yhwh">Lord</span>, how i am wronged;<br><!-- 59/60: same opening word -->
+      Witness, O <span class="yhwh">Lord</span>, how I am wronged;<br><!-- 59/60: same opening word -->
          do justice for me.
       </p>
       <p>
