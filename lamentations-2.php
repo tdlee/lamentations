28278fe17b3e78402e2ba4010116500acb98fc82
David Lee <t.d.lee@servicemusic.org.uk>
2022-08-20 14:24:48 +0100

    chap2: intro: highlight the "A to Z" to "the day of [his] anger"

--- lamentations-2.php
+++ lamentations-2.php
@@ -30,7 +30,8 @@
 
     <p>
       This second poem leaves no doubt of the horrifying prospect that God himself is the antagonist: that he has turned against his own Daughter Zion.
-      Bookended by "the day of the <span class="yhwh">Lord</span>'s anger", it is the polar opposite of the psalmist's praise,
+      Bookended, "A&nbsp;to&nbsp;Z", by "the day of the <span class="yhwh">Lord</span>'s anger",
+      this is the polar opposite of the psalmist's praise,
         <a href="http://www.usccb.org/bible/psalms/118?24" target="_blank">118:24&ndash;27</a>;
       it is the nightmare incarnation of the prophet's warning,
         <a href="http://www.usccb.org/bible/amos/5?18" target="_blank">Amos&nbsp;5:18&ndash;20</a>.
