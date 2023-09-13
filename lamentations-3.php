798f21c07334b05c505e0716c80501fdc96bffcb
David Lee <t.d.lee@servicemusic.org.uk>
2023-09-13 09:58:26 +0100

    3:19: after yesterday's rewrite, some relatively minor adjustment

--- lamentations-3.php
+++ lamentations-3.php
@@ -249,8 +249,8 @@
               $fn2='In the Hebrew text both 19 and 20 start with an emphatic verb "remember". In 20, this is doubled, albeit in different conjugations.  So "<sup>19</sup>Remember&hellip; <sup>20</sup>Remember, remember&hellip;". Our English acrostic would require a synonymous \'G\' verb.  No such verb seems apparent.  To retain this structure and emphasis, we position the verb at the verse-ends.';
             ?>
             <p>
-              Gall and bitterness<?php Footnote($fn1); ?> in such affliction,<br><!-- vv.15,19: bitter/gall word-pair -->
-                such hollowness, such remembering;
+              Gall and bitterness<?php Footnote($fn1); ?> are this affliction<br><!-- vv.15,19: bitter/gall word-pair -->
+                and hollowness&mdash;such remembering;
             </p>
             <p>
               Grief downcasts my soul&mdash;<br>
