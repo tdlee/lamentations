368dd29ce90804e9ba440ae45ed91f126fdf8a29
David Lee <t.d.lee@servicemusic.org.uk>
2023-09-16 21:23:12 +0100

    2:10: add footnote

--- lamentations-2.php
+++ lamentations-2.php
@@ -266,12 +266,13 @@
             <p>
               <?php
                 $fn='The names "Jerusalem" and "Zion" are interchanged in this verse for acrostic purposes.  A rejected alternative would have been to invert the verse (lines 1-2-3 becoming 3-2-1) but that would have lost the thematic continuity from the previous verse\'s "rulers" and "prophets" into this verse\'s "elders".';
+                $fn2='From the elders, the most senior male figures, to the most junior female figures: this suggests the two extremes of the city\'s social spectrum, becoming a merism representing the entire population.  Goldingay (2022), p.103; Berlin (2004), p.71.';
               ?>
               Jerusalem-Daughter,<?php Footnote($fn); ?> your elders<br>
               &emsp;sit dumbstruck on the ground;<br>
               dust they cast on their heads<br>
               &emsp;and sackcloth they gird.<br>
-              Zion's young women bow<br>
+              Zion's young women<?php Footnote($fn2); ?> bow<br>
               &emsp;their heads to the ground.
             </p>
           </div><!-- poem__versetext -->
