52b5230f5877da6aee27ef484be17f0de4c4a865
David Lee <t.d.lee@servicemusic.org.uk>
2023-09-24 15:15:01 +0100

    4:16 improve a "face" footnote

--- lamentations-4.php
+++ lamentations-4.php
@@ -302,7 +302,7 @@
             <p>
             <?php
               $fnface1='Very few English translations capture this verse\'s double use of "face" in the Hebrew.  Young\'s Literal Translation is a notable exception.';
-              $fnface2='"The <span class="yhwh">Lord</span>\'s face" is usually a positive image, and with priestly blessing of favour: Num.&nbsp;6:24&ndash;26. But here his face operates in a hostile fashion; see Goldingay (2022), p.179. By contrast it can also be his angry face: Lev.&nbsp;26:17; see Berlin (2004), p.102, note&nbsp;\'m\'.';
+              $fnface2='"The <span class="yhwh">Lord</span>\'s face" is usually a positive image, and with priestly blessing of favour: Num.&nbsp;6:24&ndash;26. But here his face operates in a hostile fashion; see Goldingay (2022), p.179. It can be his angry face: Lev.&nbsp;26:17; see Berlin (2004), p.102, note&nbsp;\'m\'.';
             ?>
               Scattered from the <span class="yhwh">Lord</span>'s own face:<?php Footnote($fnface1); ?><?php Footnote($fnface2); ?><br>
               &emsp;no longer he takes note of them.<br><!-- variant of "notice": see also 1:11, 1:12 etc. -->
