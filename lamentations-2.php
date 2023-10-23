60e4b58edadfa24c440db499c389f0de45476f22
David Lee <t.d.lee@servicemusic.org.uk>
2023-10-23 16:47:23 +0100

    2:19-20 untranslatable pun: babes/tormented

--- lamentations-2.php
+++ lamentations-2.php
@@ -449,12 +449,13 @@
           <div class="poem__versetext">
             <p>
               <?php
-                $fn='The rare Hebrew word here represented as "nursed" is very close to the word for "apple", reinforcing the preceding "womb\'s fruit".  Dobbs-Allsopp (2012), p.15.';
+                $fn1='A horrific but untranslatable pun bites through here between the similar words for "babe" (vv.19<em>c</em>, 20<em>b</em>) and "tormented". Dobbs-Allsopp (2012), p.99.';
+                $fn2='The rare Hebrew word here represented as "nursed" is very close to the word for "apple", reinforcing the preceding "womb\'s fruit".  Dobbs-Allsopp (2012), p.15.';
               ?>
-              Who have you thus tormented?<br>
+              Who have you thus tormented?<?php Footnote($fn1); ?><br>
               &emsp;Look, <span class="yhwh">Lord</span>; notice.<br><!-- verbs match 1:11 (and reversed 1:12) -->
               Must women eat their own womb's fruit,<br>
-              &emsp;their nursed<?php Footnote($fn); ?> babes?<br><!-- mentioned in "notes" -->
+              &emsp;their nursed<?php Footnote($fn2); ?> babes?<br><!-- mentioned in "notes" -->
               Should priest and prophet be slain<br>
               &emsp;in the sanctuary of our Lord?
             </p>
