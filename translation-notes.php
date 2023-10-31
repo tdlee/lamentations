59e42e71c70daaa77d517cb68e1641b22ea23045
David Lee <t.d.lee@servicemusic.org.uk>
2023-10-31 12:11:49 +0000

    notes: move section "word choices" to later
    This section about individual single-use words had been intruding on the bigger picture of acrostics and poetics.

--- translation-notes.php
+++ translation-notes.php
@@ -361,35 +361,6 @@
       where there is reasonable agreement that its rhythm is mostly non-<i>qinah</i>, often 3&ndash;3.
     </p>
 
-    <h2>Word choices</h2>
-
-    <p>
-      Lamentations was written in contemporary language.
-      Accordingly this version sometimes uses words and terms that are relatively modern
-      or that have a modern edge.
-      Examples that carry additional relevance from recent history, particularly <abbr title="World War 2">WW2</abbr>, are:
-    </p>
-    <ul>
-      <li>
-        blitzed (2:2): imported into English from the German "blitzkrieg".
-      </li>
-      <li>
-        firestorm (4:11): an older word that gained particular currency from Allied bombing raids.
-      </li>
-      <li>
-        holocaust (2:11, 3:47,48, 4:10): while an old word, this gained specific usage following the gas-chamber slaughter of Jewish people and others.
-      </li>
-    </ul>
-
-    <p>
-      Other examples include: "zero in" (1:22), "hell-bent" (2:8), "slow-clap" (2:15), "snide-song" (3:63), "zilch" (3:64), "ziplock" (3:65), "boozed" (4:21), "up to our necks" (5:5).
-      And "ranked" (4:2) also now carries an additional modern slant of disdain.
-    </p>
-
-    <p>
-      Meanwhile the verb "zoned" (2:22) is old, whose meanings include "encicle".
-    </p>
-
     <h3>Sit/lodge/lie: a worked example</h3>
 
     <p>
@@ -527,6 +498,35 @@
       and allows the use of a suitably unsettling alliterative expression.
     </li>
     </ul>
+
+    <h2>Word choices</h2>
+
+    <p>
+      Lamentations was written in contemporary language.
+      Accordingly this version sometimes uses words and terms that are relatively modern
+      or that have a modern edge.
+      Examples that carry additional relevance from recent history, particularly <abbr title="World War 2">WW2</abbr>, are:
+    </p>
+    <ul>
+      <li>
+        blitzed (2:2): imported into English from the German "blitzkrieg".
+      </li>
+      <li>
+        firestorm (4:11): an older word that gained particular currency from Allied bombing raids.
+      </li>
+      <li>
+        holocaust (2:11, 3:47,48, 4:10): while an old word, this gained specific usage following the gas-chamber slaughter of Jewish people and others.
+      </li>
+    </ul>
+
+    <p>
+      Other examples include: "zero in" (1:22), "hell-bent" (2:8), "slow-clap" (2:15), "snide-song" (3:63), "zilch" (3:64), "ziplock" (3:65), "boozed" (4:21), "up to our necks" (5:5).
+      And "ranked" (4:2) also now carries an additional modern slant of disdain.
+    </p>
+
+    <p>
+      Meanwhile the verb "zoned" (2:22) is old, whose meanings include "encicle".
+    </p>
 
     <h2>Inclusive language</h2>
 
