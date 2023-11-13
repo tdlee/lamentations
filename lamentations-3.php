eaaf2f5c21fcd50949c307d3103499bc572c5810
David Lee <t.d.lee@servicemusic.org.uk>
2023-11-13 17:37:30 +0000

    chap3 intro: "two voices": fix page number; introduce H-P "one voice"

--- lamentations-3.php
+++ lamentations-3.php
@@ -71,8 +71,8 @@
     </p>
 
     <p>
-      Like the earlier two poems, this can also be seen as being in two primary "voices":
-      an individual <em>geber</em> (1&ndash;24 and 40&ndash;66) and a narrator (25&ndash;39).<?php Footnote('Goldingay (2022), p.125.'); ?>
+      Like the earlier two poems, this may also be seen as being in two primary "voices":
+      an individual <em>geber</em> (1&ndash;24 and 40&ndash;66) and a narrator (25&ndash;39).<?php Footnote('Goldingay (2022), p.123. But Hens-Piazza (2017), p.39 sees just one voice throughout.'); ?>
       Or, using a slightly different break-point, 22&ndash;39 may be viewed as a "Wisdom" section, reflecting on the nature of God.<?php Footnote('Berlin (2004), p.92.'); ?>
       That said, it might be noticed that the voice in 40&ndash;47 is in plural, communal form.<?php
         Footnote('Goldingay (2022), p.150; Dobbs-Allsopp (1997), p.41.');
