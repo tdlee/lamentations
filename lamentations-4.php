9f3e0d79d6b562c518f0f3492ab6b8ee769bd13e
David Lee <t.d.lee@servicemusic.org.uk>
2023-11-15 21:43:20 +0000

    4:17: opening half-line: significantly better translation

--- lamentations-4.php
+++ lamentations-4.php
@@ -352,7 +352,7 @@
               $fn='The identity of the nation which could not or would not help is unknown. One possibility is Edom. This would connect with its condemnation a few verses ahead. Berlin (2004), pp.112&ndash;113; Provan (2016), p.121.';
             ?>
             <p>
-              Teared eyes still pine; looking<br>
+              Tired out eyes scan, exhausted,<br>
               &emsp;for help, but in vain;<br>
               From our watchtowers we watched for a nation,<?php Footnote($fn); ?><br>
               &emsp;but that cannot deliver.
