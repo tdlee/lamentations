bccf45aa892b07d2097f2be5b7b6ec368557430e
David Lee <t.d.lee@servicemusic.org.uk>
2024-03-09 21:42:29 +0000

    2:20: add Berman ref to footnote

--- lamentations-2.php
+++ lamentations-2.php
@@ -489,7 +489,7 @@
             <div class="poem__versespeaker">Daughter Zion:</div>
             <p>
               <?php
-                $fn1='An untranslatable pun of horrific intensity bites through here between the similar words for "babe" (vv.19<em>c</em>, 20<em>b</em>) and "tormented". Dobbs-Allsopp (2012), p.99.';
+                $fn1='An untranslatable pun of horrific intensity bites through here between the similar words for "babe" (vv.19<em>c</em>, 20<em>b</em>) and "tormented". Dobbs-Allsopp (2012), p.99; Berman (2023), p.83.';
                 $fn2='The rare Hebrew word here represented as "nursed" is very close to the word for "apple", reinforcing the preceding "womb\'s fruit".  Dobbs-Allsopp (2012), p.15.';
               ?>
               Who have you thus tormented?<?php Footnote($fn1); ?><br><!-- 2:20, 3:51: same strong verb -->
