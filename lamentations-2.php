d90bd73d29ea75f8f6bdec047edfbaff3e873dea
David Lee <t.d.lee@servicemusic.org.uk>
2025-06-12 18:34:55 +0100

    2:4 "now poised" => "poised"
    The "now" seems unnecessary, especially in this "2" part of 3-2 qinah,
    which needs to be as tight as possible.

--- lamentations-2.php
+++ lamentations-2.php
@@ -199,7 +199,7 @@
                 $fn4='A "pour" thread runs through the chapter at vv.4, 11, 12 and 19.  See also the "disgorge" thread in chapter&nbsp;4.';
               ?>
               <div class="indent0 acrostic-line">Drawing his bow like an enemy,</div>
-              <div class="indent2">his right hand now poised<?php Footnote($fn1); ?></div><!-- "hand": vv. 3,4,7,8,(19) --><!-- vv.3,4: contrasting use of "right hand" -->
+              <div class="indent2">his right hand poised<?php Footnote($fn1); ?></div><!-- "hand": vv. 3,4,7,8,(19) --><!-- vv.3,4: contrasting use of "right hand" -->
               <div class="indent1">like a foe, he has slain those precious</div>
               <div class="indent2">in his eye;<?php Footnote($fn2); ?></div>
               <div class="indent1">on Daughter Zion's tent<?php Footnote($fn3); ?> his fury</div><!-- "tent"/"bivouac", vv.4,6: both words repeated in footnotes --><!-- 2:4; 4:11/2: "fury" -->
