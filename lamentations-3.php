0980c3e05dba937d7f51651bf41f8100aa63d4af
David Lee <t.d.lee@servicemusic.org.uk>
2025-11-30 15:42:42 +0000

    3:18: rework awkward "Fled, perished..."

--- lamentations-3.php
+++ lamentations-3.php
@@ -144,8 +144,8 @@
               <div class="indent1">he ground me in dust.</div>
               <div class="indent0 acrostic-line">Forsworn has he peace from my soul,</div><!-- 1:11,16,19; 3:17,20,24,25,51,58: "soul" --><!-- 2:7; 3:17,31: "forswear" root -->
               <div class="indent1">I forgot what is good;</div>
-              <div class="indent0 acrostic-line">Fled, perished dregs of honour and</div><!-- "perished" better than previous "gone": Dobbs-Allsopp (2012), p.114 -->
-              <div class="indent1">vestigial hope from the <span class="yhwh">Lord</span>.<?php Footnote($fn1); ?></div>
+              <div class="indent0 acrostic-line">Festered my honour and perished</div><!-- Dobbs-Allsopp (2012), p.114 "perished" (similarly "festered") better than previous "gone": -->
+              <div class="indent1">all hope from the <span class="yhwh">Lord</span>.<?php Footnote($fn1); ?></div>
           </div><!-- poem__versetext -->
         </div><!-- poem__verse -->
 
