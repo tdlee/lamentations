52888fbda9bc845010cc885812d7722ea55cd1eb
David Lee <t.d.lee@servicemusic.org.uk>
2026-06-19 11:03:36 +0100

    4:7-8: tighter; remove unnecessary "their"

--- lamentations-4.php
+++ lamentations-4.php
@@ -147,7 +147,7 @@
           <div class="poem__versetext">
               <div class="indent0 acrostic-line">Glistening once as snow, her princes:</div>
               <div class="indent2">whiter than milk,</div>
-              <div class="indent1">their limbs more ruddy than coral,</div>
+              <div class="indent1">limbs more ruddy than coral,</div>
               <div class="indent2">their beauty as sapphire;</div>
           </div><!-- poem__versetext -->
         </div><!-- poem__verse -->
@@ -158,7 +158,7 @@
           <div class="poem__versetext">
               <div class="indent0 acrostic-line">How soot-black now their faces:</div>
               <div class="indent2">unrecognised in streets,</div>
-              <div class="indent1">their skin shrivelled, taut on their bones</div>
+              <div class="indent1">skin shrivelled, taut on their bones</div>
               <div class="indent2">as dessicated wood.</div>
           </div><!-- poem__versetext -->
         </div><!-- poem__verse -->
