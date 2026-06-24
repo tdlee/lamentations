81d35134eb61cc55caf2d3e71c9b81f371dcc635
David Lee <t.d.lee@servicemusic.org.uk>
2026-06-24 12:00:49 +0100

    5:16: improve rhythm; avoid "for" which would incorrectly link to 17-18

--- lamentations-5.php
+++ lamentations-5.php
@@ -121,7 +121,7 @@
                 <div class="indent1">our dancing has turned into dirges.</div>
                   <!-- if we can't acrosti-cate, let's alliterate -->
                 <div class="indent0">The crown from our head has tumbled:</div>
-                <div class="indent1">woe to us; for we have sinned.</div>
+                <div class="indent1">woe unto us; we have sinned.</div>
             </div><!-- poem__versetext -->
           </div><!-- poem__verse -->
 
